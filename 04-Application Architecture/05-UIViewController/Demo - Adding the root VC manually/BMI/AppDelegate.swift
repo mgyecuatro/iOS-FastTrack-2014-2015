11a741cee22d2045cb1d20da0cb6b98fdfa20088
Nicholas Outram <nicholas.outram@plymouth.ac.uk>
2015-07-28 13:17:04 +0100

    Demonstrated how to instantiate the root view controller in code.

--- 04-Application Architecture/05-UIViewController/Demo - Adding the root VC manually/BMI/AppDelegate.swift
+++ 04-Application Architecture/05-UIViewController/Demo - Adding the root VC manually/BMI/AppDelegate.swift
@@ -16,6 +16,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        
+        let sb = UIStoryboard(name: "Main", bundle: nil)
+        let vc = sb.instantiateViewControllerWithIdentifier("BMIInitialViewController")
+        window?.rootViewController = vc
+        window?.makeKeyAndVisible()
         return true
     }
 
