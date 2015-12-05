3d3b77ec067a6b72f7ca138da96ae9b097e9a56d
Nicholas Outram <nicholas.outram@plymouth.ac.uk>
2015-12-05 11:48:24 +0000

    Alpha 1.0 Breadcrumbs App - for examine the application and view controller lifecycle

---
+++ 04-Application Architecture/Swift 2/Breadcrumbs/Solution/BackgroundTest/BCCurrentPositionViewController.swift
@@ -0,0 +1,372 @@
+//
+//  ViewController.swift
+//  BackgroundTest
+//
+//  Created by Nicholas Outram on 30/11/2015.
+//  Copyright © 2015 Plymouth University. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+import MapKit
+
+class BCCurrentPositionViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate, BCOptionsSheetDelegate {
+
+   //: MARK: Enumerated types
+   
+   /// The application state - "where we are in a known sequence"
+   enum AppState {
+      case WaitingForViewDidLoad
+      case RequestingAuth
+      case LiveMapNoLogging
+      case LiveMapLogging
+      
+      init() {
+         self = .WaitingForViewDidLoad
+      }
+      
+      /// Returns a dictionary with a string description of a state
+      func dict() -> [AppState : String] {
+         let dict : [AppState : String] = [
+            .WaitingForViewDidLoad : "Wating for ViewDidLoad",
+            .RequestingAuth        : "Requesting Authorisation",
+            .LiveMapNoLogging      : "Live Map with no location logging",
+            .LiveMapLogging        : "Live Map with location Logging"
+         ]
+         return dict
+      }
+   }
+   
+   /// The type of input (and its value) applied to the state machine
+   enum AppStateInputSource {
+      case None
+      case Start
+      case AuthorisationStatus(Bool)
+      case UserWantsToStart(Bool)
+   }
+   
+   // MARK: Outlets
+   @IBOutlet weak var startButton: UIBarButtonItem!
+   @IBOutlet weak var stopButton: UIBarButtonItem!
+   @IBOutlet weak var optionsButton: UIBarButtonItem!
+   @IBOutlet weak var mapView: MKMapView!
+
+   // MARK: Properties
+   
+   /// Appliction state. Important to manage the possible sequence of events that can occur in an app life cycle
+   /// - warning: Should only be set via the `updateStateWithInput()` method
+   private var state : AppState     = AppState() {
+      willSet {
+         //Useful logging info
+         if let strOldState = state.dict()[state], strNewState = state.dict()[newValue] where strOldState != strNewState {
+            print("Changing from state \(strOldState) to state \"\(strNewState)\"")
+         }
+      }
+      didSet {
+         //If the state changes, so the output changes immediately
+         self.updateOutputWithState()
+      }
+   }
+   
+   ///Default set of options (backed by user defaults)
+   private var options : BCOptions = BCOptions() {
+      didSet {
+         //Persist the options to user defaults
+         options.updateDefaults()
+      }
+   }
+
+   ///This property is "lazy". Only runs once, when the property is first referenced
+   lazy var locationManager : CLLocationManager = {
+      print("Instantiating and initialising the location manager \"lazily\"")
+      print("This will only run once")
+      let loc = CLLocationManager()
+      
+      //Set up location manager with defaults
+      loc.desiredAccuracy = kCLLocationAccuracyBest
+      loc.distanceFilter = kCLDistanceFilterNone
+      loc.delegate = self
+      
+      //Optimisation of battery
+      loc.pausesLocationUpdatesAutomatically = true
+      loc.activityType = CLActivityType.Fitness
+      loc.allowsBackgroundLocationUpdates = false
+      
+      return loc
+   }()               //Note the parenthesis
+   
+   //Internal flag for deferred updates
+   var deferringUpdates : Bool = false
+   
+   // ************************
+   // MARK: Class Initialisers
+   // ************************
+   
+   override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+      print("\(__FILE__), \(__FUNCTION__)")
+      super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+   }
+   required init?(coder aDecoder: NSCoder) {
+      print("\(__FILE__), \(__FUNCTION__)")
+      super.init(coder: aDecoder)
+   }
+   
+   // MARK: Bringing the controller up
+   override func awakeFromNib() {
+      print("\(__FILE__), \(__FUNCTION__)")
+   }
+   
+   // *******************************************
+   // MARK: UIViewController specific lifecycle
+   // *******************************************
+   
+   override func viewDidLoad() {
+      print("\(__FILE__), \(__FUNCTION__)")
+      super.viewDidLoad()
+      
+      // Do any additional setup after loading the view, typically from a nib.
+      
+      //Set initial app state - this kicks off everything else
+      self.updateStateWithInput(.Start)
+   }
+   
+   override func viewWillAppear(animated: Bool) {
+      print("\(__FILE__), \(__FUNCTION__)")
+   }
+   override func viewDidAppear(animated: Bool) {
+      print("\(__FILE__), \(__FUNCTION__)")
+   }
+   
+   // MARK: Tearing the controller down
+   override func viewWillDisappear(animated: Bool) {
+      print("\(__FILE__), \(__FUNCTION__)")
+   }
+   override func viewDidDisappear(animated: Bool) {
+      print("\(__FILE__), \(__FUNCTION__)")
+   }
+   deinit {
+      print("\(__FILE__), \(__FUNCTION__)")
+   }
+   
+   // ************************
+   // MARK: Events and Actions
+   // ************************
+   
+   override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+      print("\(__FILE__), \(__FUNCTION__)")
+   }
+   
+   func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+      print("Authorisation changed to \(status.rawValue)")
+      if status == CLAuthorizationStatus.AuthorizedAlways {
+         print("Authorisation granted")
+         self.updateStateWithInput(.AuthorisationStatus(true))
+      } else {
+         self.updateStateWithInput(.AuthorisationStatus(false))
+      }
+   }
+   
+   @IBAction func doStart(sender: AnyObject) {
+      self.updateStateWithInput(.UserWantsToStart(true))
+   }
+
+   @IBAction func doStop(sender: AnyObject) {
+      self.updateStateWithInput(.UserWantsToStart(false))
+   }
+   
+   //Called before a segue performs navigation
+   override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+      print("\(__FILE__), \(__FUNCTION__)")
+      
+      //Pass data forwards to the *presented* view controller
+      if segue.identifier == "ModalOptions" {
+         if let dstVC = segue.destinationViewController as? BCOptionsTableViewController {
+            dstVC.delegate = self
+            dstVC.options = self.options  //Pass a COPY
+         }
+      }
+   }
+   
+   // ****************************
+   // MARK: BCOptionsSheetDelegate
+   // ****************************
+   
+   func dismissWithUpdatedOptions(updatedOptions: BCOptions?) {
+      print("\(__FILE__), \(__FUNCTION__)")
+      //If saved, updatedOptions will have a wrapped copy
+      if let op = updatedOptions {
+         self.options = op             //Make copy
+
+         //Update the application UI and Location manager
+         updateOutputWithState()
+      }
+      
+      //Using a trailing closure syntax, dismiss the presented and run completion handler
+      self.dismissViewControllerAnimated(true) {
+         print("Presented animation has now completed")
+      }
+   }
+   
+   
+   // *******************************
+   // MARK: CLLocationManagerDelegate
+   // *******************************
+
+   func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+      print("Location manager failed \(error)")
+   }
+   
+   func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+      guard let location = locations.last else { return }
+      
+      print("\(locations.count) Location(s) Updated: \(location)")
+
+      /// TODO: test this power saving option
+//      if (!self.deferringUpdates) {
+//         manager.allowDeferredLocationUpdatesUntilTraveled(self.options.distanceBetweenMeasurements*100.0, timeout: CLTimeIntervalMax)
+//         self.deferringUpdates = true
+//      }
+      
+   }
+   
+   func locationManager(manager: CLLocationManager, didFinishDeferredUpdatesWithError error: NSError?) {
+      print("Finished deferring updates")
+      self.deferringUpdates = false
+   }
+  
+   // ***********************
+   // MARK: MKMapViewDelegate
+   // ***********************
+   
+   // Moving the map will reset the user tracking mode. I force it back
+   func mapView(mapView: MKMapView, didChangeUserTrackingMode mode: MKUserTrackingMode, animated: Bool) {
+      mapView.userTrackingMode = .Follow
+   }
+   
+   // **************************
+   // MARK: Finite State Machine
+   // **************************
+   
+   //UPDATE STATE
+   func updateStateWithInput(ip : AppStateInputSource)
+   {
+      var nextState = self.state
+      
+      switch (self.state) {
+      case .WaitingForViewDidLoad:
+         
+         //Check inputs type
+         if case .Start = ip  {
+            nextState = .RequestingAuth
+         }
+         
+      case .RequestingAuth:
+         //Ensure user has given permission  to access location
+
+         //Check inputs type and associated value
+         if case .AuthorisationStatus(let val) = ip where val == true {
+            nextState = .LiveMapNoLogging
+         }
+         
+      case .LiveMapNoLogging:
+         // Authorisation is given
+         
+         //Check for user cancelling permission
+         if case .AuthorisationStatus(let val) = ip where val == false {
+            nextState = .RequestingAuth
+         }
+         //Check for start button
+         else if case .UserWantsToStart(let val) = ip where val == true {
+            nextState = .LiveMapLogging
+         }
+      
+      case .LiveMapLogging:
+         
+         if case .AuthorisationStatus(let val) = ip where val == false {
+            nextState = .RequestingAuth
+         }
+         else if case .UserWantsToStart(let val) = ip where val == false {
+            nextState = .LiveMapNoLogging
+         }
+         
+      } //end switch
+      
+      //State Transition (will trigger updates in the output)
+      self.state = nextState
+   }
+   
+   //UPDATE (MOORE) OUTPUTS
+   func updateOutputWithState() {
+      switch (self.state) {
+      case .WaitingForViewDidLoad:
+         break
+         
+      case .RequestingAuth:
+         
+         //Request authorisation from the user to log location in the background
+         self.locationManager.requestAlwaysAuthorization()
+         
+         //Set UI into default state until authorised
+         
+         //Buttons
+         startButton.enabled = false
+         stopButton.enabled = false
+         optionsButton.enabled = false
+         
+         //Map defaults (pedantic)
+         mapView.showsUserLocation = false
+         mapView.delegate = nil
+         
+         //Location manger (pedantic)
+         locationManager.stopUpdatingLocation()
+         locationManager.allowsBackgroundLocationUpdates = false
+         
+      case .LiveMapNoLogging:
+         // We now have permission to use location
+         
+         //Buttons for logging
+         startButton.enabled = true
+         stopButton.enabled = false
+         optionsButton.enabled = true
+         
+         //Live Map
+         mapView.showsUserLocation = true
+         mapView.userTrackingMode = .Follow
+         mapView.rotateEnabled = self.options.headingUP
+         mapView.showsTraffic = self.options.showTraffic
+         mapView.delegate = self
+         
+         //Set Location manger state
+         locationManager.desiredAccuracy = self.options.gpsPrecision
+         locationManager.distanceFilter = self.options.distanceBetweenMeasurements
+         locationManager.allowsBackgroundLocationUpdates = false
+         locationManager.stopUpdatingLocation()
+         
+         
+      case .LiveMapLogging:
+         //Buttons
+         startButton.enabled = false
+         stopButton.enabled = true
+         optionsButton.enabled = true
+         
+         //Map
+         mapView.showsUserLocation = true
+         mapView.userTrackingMode = .Follow
+         mapView.rotateEnabled = self.options.headingUP
+         mapView.showsTraffic = self.options.showTraffic
+         mapView.delegate = self
+         
+         //Location manger
+         locationManager.desiredAccuracy = self.options.gpsPrecision
+         locationManager.distanceFilter = self.options.distanceBetweenMeasurements
+         locationManager.allowsBackgroundLocationUpdates = self.options.backgroundUpdates
+         locationManager.startUpdatingLocation()
+         
+
+      } //end switch
+   }
+   
+}
+
---
+++ 04-Application Architecture/Swift 2/Breadcrumbs/Solution/BackgroundTest/BCOptions.swift
@@ -0,0 +1,72 @@
+//
+//  BCOptions.swift
+//  Breadcrumbs
+//
+//  Created by Nicholas Outram on 04/12/2015.
+//  Copyright © 2015 Plymouth University. All rights reserved.
+//
+
+import Foundation
+
+/// A structure that encapsualtes the functionality for passing, updating and persisting the options for the Bread Crumbs app
+/// - note: If there are two copies, it is not clear if races can occur when writing userdefaults with `updateDefaults()`. It is reccomended you only call `updateDefaults()` on one copy OR if you do, call `commit()` immediately after.
+/// - author: Nicholas Outram, Plymouth University
+/// - version: 1.0
+
+struct BCOptions {
+   
+   //This structure uses NSUserDefaults - designed to store a heirarchy of Foundation Class objects (NSNumber in this case)
+   //Swift types Bool and Float will be bridged automatically to Foundation class types
+   
+   /// Factory defaults - if no user default is set, these will be used
+   static let defaultsDictionary : [String : AnyObject] = [
+      "backgroundUpdates" : false,
+      "headingUP" : false,
+      "showTraffic" : false,
+      "distanceBetweenMeasurements" : 100.0,
+      "gpsPrecision" : 10.0
+   ]
+   
+   ///Access the standardUserDefaults via this property to ensure factory settings (registration domain) are initialised.
+   ///In the event that no userdefaults have been set yet, it will fall back the factory default.
+   static var defaults : NSUserDefaults = { () -> NSUserDefaults in
+      //Initiaised lazily (in part because it is static) using a closure.
+      let ud = NSUserDefaults.standardUserDefaults()
+      ud.registerDefaults(BCOptions.defaultsDictionary)
+      ud.synchronize()
+      return ud
+   }() // Note the parenthesis on the end
+    
+   
+   // Public properties - these are value-type variable backed, so independent copies can be made
+   // These parameters are NOT static and are storage backed. 
+   // I want to exploit the value-type semantics as I pass this forward through the navigation stack
+   // They are all initalised using the userdefaults
+   
+   /// determines if the GPS will continue to log data when the app is running in the background. Has an impact on battery drain.
+   lazy var backgroundUpdates : Bool            = BCOptions.defaults.boolForKey("backgroundUpdates")
+   /// determines if the map is rotated so the heading is always towards the top of the screen
+   lazy var headingUP : Bool                    = BCOptions.defaults.boolForKey("headingUP")
+   /// determines if the traffic is shown on the map
+   lazy var showTraffic : Bool                  = BCOptions.defaults.boolForKey("showTraffic")
+   /// distance (in meters) you must travel before another GPS location is reported. A higher value may results in extended battery time
+   lazy var distanceBetweenMeasurements : Double = BCOptions.defaults.doubleForKey("distanceBetweenMeasurements")
+   /// requried prevision of the GPS (in meters). Higher values are less specific, but are faster to obtain and may require less battery
+   lazy var gpsPrecision : Double                = BCOptions.defaults.doubleForKey("gpsPrecision")
+
+ 
+   /// Save the current record set to userdefaults (note - userdefaults may be cached, so call commit before allowing the application to close)
+   mutating func updateDefaults() {
+      BCOptions.defaults.setBool(backgroundUpdates, forKey: "backgroundUpdates")
+      BCOptions.defaults.setBool(headingUP, forKey: "headingUP")
+      BCOptions.defaults.setBool(showTraffic, forKey: "showTraffic")
+      BCOptions.defaults.setDouble(distanceBetweenMeasurements, forKey: "distanceBetweenMeasurements")
+      BCOptions.defaults.setDouble(gpsPrecision, forKey: "gpsPrecision")
+   }
+   
+   /// Given that userdefaults are cached and only saved periodically, calling this forces them to be saved
+   static func commit() {
+      BCOptions.defaults.synchronize()
+   }
+   
+}
