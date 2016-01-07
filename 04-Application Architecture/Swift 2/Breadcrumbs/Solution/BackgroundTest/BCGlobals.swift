ca6273b92dee13fdf1dd2b7af8fc47f4d1b95c53
Nicholas Outram <nicholas.outram@plymouth.ac.uk>
2016-01-07 12:14:33 +0000

    Updated document path util functions

--- 04-Application Architecture/Swift 2/Breadcrumbs/Solution/BackgroundTest/BCGlobals.swift
+++ 04-Application Architecture/Swift 2/Breadcrumbs/Solution/BackgroundTest/BCGlobals.swift
@@ -8,4 +8,12 @@
 
 import Foundation
 let globalDocumentsPath = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true).first
+
+func pathForDocument(filename : String) -> String? {
+   guard let gPath = globalDocumentsPath else {
+      return nil
+   }
+   
+   return NSString(string: gPath).stringByAppendingPathComponent(filename) as String
+}
 //let globalPath = globalDocumentsPath?.stringByAppendingString("/Options")
